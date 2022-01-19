29dc44fc43b064a4db775ad480b22cc4ddf99d06
Szymon-T1 <trybuch.szymon@gmail.com>
2022-01-19 15:10:41 +0100

    WIP

--- testCases.js
+++ testCases.js
@@ -1,24 +1,3 @@
-let d = function (schema, state) {
+let a = require("./tests/invalid.json");
 
-}
-
-let bla = {
-    key__v: {
-        variant1: {
-            key1: "String",
-            key2: "String"
-        },
-        variant2: {
-            key1: "String",
-            key2: "String",
-            key3: {
-                key3_1: "String",
-                key3_2: "String",
-                key3_3: "String"
-            }
-        }
-    }
-}
-
-
-console.log(JSON.stringify(bla, 0, 2))
+console.log(a)
